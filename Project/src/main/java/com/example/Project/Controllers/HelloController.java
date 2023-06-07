5381d941cbbcfffd69741c58c8f49837a8cd3f0d
KmnPetr <kmn.petrichenko@yandex.ru>
2023-06-08 00:09:51 +0300

    Project

---
+++ Project/src/main/java/com/example/Project/Controllers/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.Project.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String helloPage(){
+        return "hello";
+    }
+}
